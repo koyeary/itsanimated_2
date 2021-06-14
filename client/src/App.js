cd7bb734a32291528a9b2ec43e69b5b5465f1cb0
koyeary <katyeary@gmail.com>
2021-06-13 19:40:08 -0700

    set up basic redux (store, auth actions and reducers, root reducer, utils/api)

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/navs/Header';
 import Home from './components/pages/Home';
@@ -6,29 +6,32 @@ import Home from './components/pages/Home';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
-//import { loadUser } from './actions/auth';
+import { loadUser } from './redux/auth/actions/authActions';
 import setAuthToken from './utils/setAuthToken';
-//import { LOGOUT } from './actions/types';
+import { LOGOUT } from './redux/auth/actions/types';
 
 const App = () => {
-   useEffect(() => {
+  useEffect(() => {
     // check for token in LS
     if (localStorage.token) {
       setAuthToken(localStorage.token);
-    }
-   // store.dispatch(loadUser());
 
-    // log user out from all tabs if they log out in one tab
-    window.addEventListener('storage', () => {
-      if (!localStorage.token) store.dispatch({ /* type: LOGOUT */ });
-    });
-  }, []); 
+      store.dispatch(loadUser());
+
+      // log user out from all tabs if they log out in one tab
+      window.addEventListener('storage', () => {
+        if (!localStorage.token) store.dispatch({ type: LOGOUT });
+      });
+    }
+  }, []);
 
   return (
+    <Provider store={store}>
       <Router>
-              <Header/>
-              <Home/>
+        <Header />
+        <Home />
       </Router>
+    </Provider>
   );
 };
 
---
+++ client/src/redux/rootReducer.js
@@ -0,0 +1,19 @@
+
+import { combineReducers } from 'redux';
+//import alert from './alert';
+import auth from './auth/reducers/authReducer';
+/* import shop from './shop';
+import images from './images';
+import stripe from './stripe';
+import cart from './cart';
+import contact from './contact'; */
+
+export default combineReducers({
+    // alert
+      auth,
+     /*shop,
+     images,
+     stripe,
+     cart, 
+     contact */
+});
