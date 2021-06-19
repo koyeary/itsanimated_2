083157b208685bf076cf80aba066a7487c0a34aa
koyeary <katyeary@gmail.com>
2021-06-19 11:54:34 -0700

    add stashed changes (Details component) from tumblr2

--- client/src/components/shop/userStorefront/Product.js
+++ client/src/components/shop/userStorefront/Product.js
@@ -2,45 +2,44 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { getProducts } from '../../../redux/shop/actions/shopActions';
 import { connect } from 'react-redux';
+import Details from './components/Details';
 
-const Product = ({ getProducts, shop: { products }}) => {
+const Product = ({ getProducts, shop: { products } }) => {
+  useEffect(() => getProducts());
 
-    useEffect(() => getProducts());
-
-    return (
-        <div className="Product-wrapper">
-			{products &&
-				products.map((product, i) => {
-					//const image = product.images[0]
-					return (
-						<div className="Product" key={product._id}>
-{/* 							{image ? (
+  return (
+    <div className='Product-wrapper'>
+      {products &&
+        products.map((product, i) => {
+          //const image = product.images[0]
+          return (
+            <div className='Product' key={product._id}>
+              {/* 							{image ? (
 								<img src={product.image_src} alt={`${product.name} product shot`} />
 							) : null} */}
-							<div> 
-								<h4 className="Product__title">{product.name}</h4>
-								<p className="Product__price">${product.price}</p>
-							</div>
-							<button
-								className="Product__buy button"
-								onClick={console.log(product._id)}
-							>
-								View Details
-							</button>
-						</div>
-					)
-				})}
-		</div>
-    )
-}
+              <div>
+                <h4 className='Product__title'>{product.name}</h4>
+                <p className='Product__price'>${product.price}</p>
+              </div>
+              <Details
+                name={product.name}
+                price={product.price}
+                image={product.image_src}
+              />
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 
 Product.propTypes = {
-    getProducts: PropTypes.func.isRequired,
-    shop: PropTypes.object.isRequired
-  };
-  
-  const mapStateToProps = (state) => ({
-    shop: state.shop
-  });
-  
-  export default connect(mapStateToProps, { getProducts })(Product);
+  getProducts: PropTypes.func.isRequired,
+  shop: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  shop: state.shop
+});
+
+export default connect(mapStateToProps, { getProducts })(Product);
