3711e5097101a0ac2c6a0d318af56d583035429f
koyeary <katyeary@gmail.com>
2021-06-19 12:18:18 -0700

    backend api initiates checkout session using req.body

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -2,7 +2,7 @@ const config = require('config');
 const stripe = require('stripe')(config.get('stripe_key'));
 
 module.exports = {
-/*   createCustomer: async (req, res) => {
+  /*   createCustomer: async (req, res) => {
     const { email, cart } = req.body;
 
     const customer = await stripe.customers.create({
@@ -13,26 +13,34 @@ module.exports = {
   }, */
 
   createCheckoutSession: async (req, res) => {
-    // const { quantity, name, images }
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'Stubborn Attachments',
-              images: ['https://i.imgur.com/EHyR2nP.png'] //preview purchase
+    const { quantity, name, images, price } = req.body;
+
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name,
+                images //preview purchase
+              },
+              unit_amount: 2000
             },
-            unit_amount: 2000
-          },
-          quantity: 1
-        }
-      ],
-      mode: 'payment',
-      success_url: `http://localhost:5000`, //purchase successful page
-      cancel_url: `http://localhost:8080`   //purchase cancelled page
-    });
-    return res.json({ id: session.id });
+            quantity
+          }
+        ],
+        mode: 'payment',
+        success_url: `http://localhost:5000`, //purchase successful page
+        cancel_url: `http://localhost:8080` //purchase cancelled page
+      });
+
+      return res.json({ id: session.id });
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 };
