e1a42790a1541bf2b4df095f679a88ca0a592342
koyeary <katyeary@gmail.com>
2021-08-20 17:39:42 -0700

    clean up ProductCard component

--- client/src/components/shop/product/Details.js
+++ client/src/components/shop/product/Details.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
 
 const Details = ({ name, price }) => {
+    
     return (
         <Fragment>
             <h4 className='Product__title'>{name}</h4>
-                <p className='Product__price'>${price}</p>
+            <p className='Product__price'>${price}</p>
         </Fragment>
     )
 }
--- client/src/components/shop/product/ProductCard.js
+++ client/src/components/shop/product/ProductCard.js
@@ -1,28 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { getProducts } from '../../../redux/inventory/actions/inventoryActions';
-import { addToCart } from '../../../redux/cart/actions/cartActions';
-import { connect } from 'react-redux';
 import Image from './Image';
 import Details from './Details';
 import Actions from './Actions';
-import { Button, Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+
+import { getProducts } from '../../../redux/inventory/actions/inventoryActions';
+import { addToCart } from '../../../redux/cart/actions/cartActions';
+import { connect } from 'react-redux';
 
 const ProductCard = ({ getProducts, inventory: { products } }) => {
   useEffect(() => getProducts());
 
   return (
     <Card className='Product-wrapper h-100'>
-      {products &&
-        products.map((product, i) => {
-          return (
-            <div className='Product' key={product._id}>
-              <Image image={product.image_src} />
-              <Details name={product.name} price={product.price} />
-              <Card.Footer><Actions addToCart={addToCart} product={product}/></Card.Footer>
-            </div>
-          );
-        })}
+      {products.map((product, i) => {
+        return (
+          <div className='Product' key={product._id}>
+            <Image image={product.image_src} />
+            <Details name={product.name} price={product.price} />
+            <Card.Footer>
+              <Actions addToCart={addToCart} product={product} />
+            </Card.Footer>
+          </div>
+        );
+      })}
     </Card>
   );
 };
